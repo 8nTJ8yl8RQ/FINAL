be4219e855f20084ba2bbeaa89ed8b1553908f7c
Teoderito Carmona <jun.carmona@gmail.com>
2011-10-27 05:50:54 -0400

    secure pages of athletes

--- athlete/athleteAdd.php
+++ athlete/athleteAdd.php
@@ -1,3 +1,11 @@
+<?php
+	require_once "../User/include/session.php";
+	
+	if(!($session->logged_in)){
+		Location("header : ../User/main.php");
+		exit(1);
+	}
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
